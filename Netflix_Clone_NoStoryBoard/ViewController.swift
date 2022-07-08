27e2416fa8b3c6c6bbe2d6a377cc8cb00486a0f0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-08 17:23:25 +0900

    Initial Commit

---
+++ Netflix_Clone_NoStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone_NoStoryBoard
+//
+//  Created by 이창형 on 2022/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
